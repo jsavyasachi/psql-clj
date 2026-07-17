6940c0146ac36ca84ea2bc73503bedddcc4689cc
Savyasachi <jsavyasachi@gmail.com>
2026-07-16 21:09:43 -0700

    fix(pgpass): parse escaped fields and normalize matching

--- src/psql/pgpass.clj
+++ src/psql/pgpass.clj
@@ -4,12 +4,30 @@
             [clojure.string :as str]))
 
 (defn parse-pgpass-line
-  "The .pgpass files has lines of format: hostname:port:database:username:password
-  Return a map of fields {:pg-hostname \"*\" ...}"
+  "The .pgpass file has lines of format: hostname:port:database:username:password.
+  Return a map of fields {:pg-hostname \"*\" ...}, or nil for an ignored line."
   [s]
-  (zipmap
-   [:pg-hostname :pg-port :pg-database :pg-username :pg-password]
-   (str/split s #":")))
+  (when-not (or (str/blank? s) (re-find #"^\\s*#" s))
+    (let [fields (loop [remaining (seq s)
+                        field (StringBuilder.)
+                        result []]
+                   (if-let [c (first remaining)]
+                     (let [next-c (second remaining)]
+                       (cond
+                         (and (= c \\) (#{\: \\} next-c))
+                         (recur (nnext remaining) (.append field next-c) result)
+
+                         (= c \:)
+                         (recur (next remaining) (StringBuilder.)
+                                (conj result (str field)))
+
+                         :else
+                         (recur (next remaining) (.append field c) result)))
+                     (conj result (str field))))]
+      (when (= 5 (count fields))
+        (zipmap
+         [:pg-hostname :pg-port :pg-database :pg-username :pg-password]
+         fields)))))
 
 (defn read-pgpass
   "Find ~/.pgpass, read it and parse lines into maps"
@@ -26,17 +44,22 @@
 (defn pgpass-matches?
   "(filter (partial pgpass-matches? spec) pgpass-lines)"
   [{:keys [host port dbname user]} {:keys [pg-hostname pg-port pg-database pg-username pg-password]}]
-  (when
-   (and
-    (or (= pg-hostname "*") (= pg-hostname host) (and (= pg-hostname "localhost") (nil? host)))
-    (or (= pg-port "*") (= pg-port port) (and (= pg-port "5432") (nil? port)))
-    (or (= pg-database "*") (= pg-database dbname))
-    (or (= pg-username "*") (= pg-username user)))
-    pg-password))
+  (let [local-host? (or (nil? host)
+                        (= "" host)
+                        (and (string? host) (.isAbsolute (io/file host))))
+        port (str (or port 5432))]
+    (when
+     (and
+      (or (= pg-hostname "*")
+          (= pg-hostname host)
+          (and (= pg-hostname "localhost") local-host?))
+      (or (= pg-port "*") (= (str pg-port) port))
+      (or (= pg-database "*") (= pg-database dbname))
+      (or (= pg-username "*") (= pg-username user)))
+      pg-password)))
 
 (defn pgpass-lookup
   "Look up password from ~/.pgpass based on db spec {:host ... :port ... :dbname ... :user ...}"
   [spec]
   (when-let [match (first (filter (partial pgpass-matches? spec) (read-pgpass)))]
     (:pg-password match)))
-
--- test/psql/pgpass_test.clj
+++ test/psql/pgpass_test.clj
@@ -5,7 +5,19 @@
 (deftest parse-pgpass-line
   (is (= {:pg-hostname "localhost" :pg-port "5432" :pg-database "mydb"
           :pg-username "me" :pg-password "secret"}
-         (pgpass/parse-pgpass-line "localhost:5432:mydb:me:secret"))))
+         (pgpass/parse-pgpass-line "localhost:5432:mydb:me:secret")))
+  (testing "escaped colons and backslashes survive parsing"
+    (is (= {:pg-hostname "local:host" :pg-port "5432" :pg-database "mydb"
+            :pg-username "me" :pg-password "colon:and\\slash"}
+           (pgpass/parse-pgpass-line
+            "local\\:host:5432:mydb:me:colon\\:and\\\\slash"))))
+  (testing "malformed records are ignored"
+    (is (nil? (pgpass/parse-pgpass-line "localhost:5432:mydb:me")))
+    (is (nil? (pgpass/parse-pgpass-line "localhost:5432:mydb:me:pw:extra"))))
+  (testing "blank and comment lines are ignored"
+    (is (nil? (pgpass/parse-pgpass-line "")))
+    (is (nil? (pgpass/parse-pgpass-line "   ")))
+    (is (nil? (pgpass/parse-pgpass-line "  # comment")))))
 
 (deftest pgpass-matches?
   (testing "all-wildcard line matches and yields the password"
@@ -18,6 +30,17 @@
                  {:host nil :port nil :dbname "d" :user "u"}
                  {:pg-hostname "localhost" :pg-port "5432" :pg-database "*"
                   :pg-username "*" :pg-password "pw"}))))
+  (testing "ports are normalized"
+    (is (= "pw" (pgpass/pgpass-matches?
+                 {:host "h" :port 5432 :dbname "d" :user "u"}
+                 {:pg-hostname "h" :pg-port "5432" :pg-database "d"
+                  :pg-username "u" :pg-password "pw"}))))
+  (testing "localhost matches empty and absolute socket hosts"
+    (doseq [host ["" "/var/run/postgresql"]]
+      (is (= "pw" (pgpass/pgpass-matches?
+                   {:host host :port "5432" :dbname "d" :user "u"}
+                   {:pg-hostname "localhost" :pg-port "5432" :pg-database "d"
+                    :pg-username "u" :pg-password "pw"})))))
   (testing "username mismatch does not match"
     (is (not (pgpass/pgpass-matches?
               {:host "h" :port "5432" :dbname "d" :user "u"}
